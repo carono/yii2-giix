63b9a56d9efe977456665735372df12de5baaf50
carno <spamer2000@list.ru>
2016-12-04 02:09:26 +0500

    refs

--- GiixController.php
+++ GiixController.php
@@ -3,7 +3,6 @@
 namespace carono\giix;
 
 use schmunk42\giiant\commands\BatchController;
-use schmunk42\giiant\generators\model\Generator;
 
 class GiixController extends BatchController
 {
@@ -16,7 +15,7 @@ class GiixController extends BatchController
 
     public function init()
     {
-        if (in_array('common',\Yii::$aliases)){
+        if (in_array('common', \Yii::$aliases)) {
             \Yii::getAlias('@common');
             if ($this->modelNamespace == 'app\models') {
                 $this->modelNamespace = 'common\models';
@@ -32,33 +31,36 @@ class GiixController extends BatchController
     protected function getYiiConfiguration()
     {
         $config = parent::getYiiConfiguration();
+        $class = 'schmunk42\giiant\generators\model\Generator';
+        $name = 'giiant-model';
+        $template = '@vendor/carono/yii2-giix/templates/model';
 
-        $config['modules']['gii'] = [
-            'class'      => 'yii\gii\Module',
-            'generators' => [
-                'giiant-model' => [
-                    'class'     => Generator::className(),
-                    'templates' => [
-                        'caronoModel' => '@vendor/carono/yii2-giix/templates/model'
-                    ]
-                ]
-            ]
-        ];
+        self::addTemplateToGiiGenerator($config, $class, $name, $template);
+
         return $config;
     }
 
-    public static function addTemplateToGiiGenerator($generator, $name, $template)
+    public static function addTemplateToGiiGenerator(&$config, $generator, $name, $template)
     {
-        if ($gii = \Yii::$app->getModule('gii')) {
-            $gii->generators[$generator]["templates"][$name] = $template;
-        }
+        self::prepareGii($config);
+
+        $config['modules']['gii']['generators'][$name] = [
+            'class'     => $generator,
+            'templates' => [
+                'caronoModel' => $template
+            ]
+        ];
     }
 
-    public static function addGeneratorToGii($name, $class)
+    protected static function prepareGii(&$config)
     {
-        \Yii::$app->getModule('gii')->generators[$name] = [
-            "class"     => $class,
-            "templates" => []
-        ];
+        if (!is_array($config['modules']['gii'])) {
+            $config['modules']['gii'] = [
+                'class'      => 'yii\gii\Module',
+                'generators' => []
+            ];
+        } elseif (isset($config['modules']['gii']['generators'])) {
+            $config['modules']['gii']['generators'] = [];
+        }
     }
 }
